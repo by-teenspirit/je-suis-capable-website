7ee4bc3c90ed6aa3121cda58c182a680867fbff9
teenspirit <90917286+by-teenspirit@users.noreply.github.com>
2023-05-04 14:12:47 +0200

    Nouveau caroussel

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
-const track = document.querySelector('.track');
 const cards = Array.from(document.querySelectorAll('.card-container'));
-const cardWidth = cards[0].offsetWidth;
-const trackWidth = cardWidth * cards.length;
-const container = document.querySelector('.carousel-container');
 const nav = document.querySelector('.nav');
 const prevBtn = document.querySelector('.prev');
 const nextBtn = document.querySelector('.next');
 let currentPosition = 0;
-
-// Set the width of the track based on the total width of the cards
-track.style.width = `${trackWidth}px`;
-
-// Move the track to show the current position
-function updatePosition() {
-    track.style.transform = `translateX(-${currentPosition * cardWidth}px)`;
-}
 
 // Move to the next card
 function nextCard() {
@@ -37,8 +25,6 @@ function prevCard() {
 // Add event listeners to the prev and next buttons
 prevBtn.addEventListener('click', prevCard);
 nextBtn.addEventListener('click', nextCard);
-
-
 
 // For the second carousel
 
@@ -74,3 +60,69 @@ rightArrow.addEventListener('click', function() {
     indicatorParents.children[articleIndex].classList.add('selected');
     setIndex();
 });
+
+// For the card slider
+
+const carousel = document.querySelector(".caroussel");
+const arrowBtns = document.querySelectorAll(".wrapper span.material-symbols-outlined");
+const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const carouselChildrens = [...carousel.children];
+
+let isDragging = false,
+    startX, startScrollLeft;
+
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+// Insérer des copies des dernières cartes au début du carousel pour le scroll infini
+carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+    carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+});
+
+carouselChildrens.slice(0, cardPerView).reverse().forEach(card => {
+    carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+});
+
+// Scroll le caroussel à droite et à gauche avec les boutons
+arrowBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
+    })
+});
+
+const dragStart = (e) => {
+    isDragging = true;
+    carousel.classList.add("dragging");
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    if (!isDragging) return; // si false, recommencer ici
+    // on update la position du scroll en fonction du mouvement du curseur
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+const dragStop = () => {
+    isDragging = false;
+    carousel.classList.remove("dragging");
+}
+
+const infiniteScroll = () => {
+    // Si le caroussel est au début, scroll à la fin
+    if (carousel.scrollLeft === 0) {
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
+        carousel.classList.remove("no-transition");
+    }
+    // Si la caroussel est à la fin, scroll au début
+    else if (Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.offsetWidth;
+        carousel.classList.remove("no-transition");
+    }
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("scroll", infiniteScroll);
