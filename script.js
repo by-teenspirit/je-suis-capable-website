d9603fbd90fb25c83be431079cef19da3600573e
teenspirit <90917286+by-teenspirit@users.noreply.github.com>
2023-05-05 15:06:54 +0200

    Optimisation du code et des images

--- script.js
+++ script.js
@@ -1,70 +1,7 @@
-const cards = Array.from(document.querySelectorAll('.card-container'));
-const nav = document.querySelector('.nav');
-const prevBtn = document.querySelector('.prev');
-const nextBtn = document.querySelector('.next');
-let currentPosition = 0;
-
-// Move to the next card
-function nextCard() {
-    currentPosition++;
-    if (currentPosition > cards.length - 1) {
-        currentPosition = 0;
-    }
-    updatePosition();
-}
-
-// Move to the previous card
-function prevCard() {
-    currentPosition--;
-    if (currentPosition < 0) {
-        currentPosition = cards.length - 1;
-    }
-    updatePosition();
-}
-
-// Add event listeners to the prev and next buttons
-prevBtn.addEventListener('click', prevCard);
-nextBtn.addEventListener('click', nextCard);
-
-// For the second carousel
-
-const slider = document.querySelector('.works-slider');
-const leftArrow = document.querySelector('.prev-works');
-const indicatorParents = document.querySelector('.works-carousel ul');
-const rightArrow = document.querySelector('.next-works');
-var articleIndex = 0;
-
-function setIndex() {
-    document.querySelector('.works-carousel .selected').classList.remove('selected');
-    slider.style.transform = 'translate(' + (articleIndex) * -16.7 + '%)';
-
-}
-
-document.querySelectorAll('.works-carousel li').forEach(function(indicator, ind) {
-    indicator.addEventListener('click', function() {
-        articleIndex = ind;
-        setIndex();
-        indicator.classList.add('selected');
-
-    })
-})
-
-leftArrow.addEventListener('click', function() {
-    articleIndex = (articleIndex > 0) ? articleIndex - 1 : 0;
-    indicatorParents.children[articleIndex].classList.add('selected');
-    setIndex();
-});
-
-rightArrow.addEventListener('click', function() {
-    articleIndex = (articleIndex < 5) ? articleIndex + 1 : 5;
-    indicatorParents.children[articleIndex].classList.add('selected');
-    setIndex();
-});
-
-// For the card slider
+// Pour le card slider
 
 const carousel = document.querySelector(".caroussel");
-const arrowBtns = document.querySelectorAll(".wrapper span.material-symbols-outlined");
+const arrowBtns = document.querySelectorAll(".wrapper .icon img");
 const firstCardWidth = carousel.querySelector(".card").offsetWidth;
 const carouselChildrens = [...carousel.children];
 
@@ -88,7 +25,7 @@ arrowBtns.forEach(btn => {
         carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
     })
 });
-
+ // Fonction drag sur les images
 const dragStart = (e) => {
     isDragging = true;
     carousel.classList.add("dragging");
@@ -126,3 +63,67 @@ carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
 document.addEventListener("mouseup", dragStop);
 carousel.addEventListener("scroll", infiniteScroll);
+ 
+
+// Pour le caroussel
+const cards = Array.from(document.querySelectorAll('.card-container'));
+const nav = document.querySelector('.nav');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+let currentPosition = 0;
+
+// Aller à la carte suivante
+function nextCard() {
+    currentPosition++;
+    if (currentPosition > cards.length - 1) {
+        currentPosition = 0;
+    }
+    updatePosition();
+}
+
+// Aller à la carte précédente
+function prevCard() {
+    currentPosition--;
+    if (currentPosition < 0) {
+        currentPosition = cards.length - 1;
+    }
+    updatePosition();
+}
+prevBtn.addEventListener('click', prevCard);
+nextBtn.addEventListener('click', nextCard);
+
+// Pour que les flèches et le menu en-dessous se suivent
+
+const slider = document.querySelector('.works-slider');
+const leftArrow = document.querySelector('.prev-works');
+const indicatorParents = document.querySelector('.works-carousel ul');
+const rightArrow = document.querySelector('.next-works');
+var articleIndex = 0;
+
+// Quand on clique sur la flèche, on déplace en même temps le sélecteur sur le menu 
+function setIndex() {
+    document.querySelector('.works-carousel .selected').classList.remove('selected');
+    slider.style.transform = 'translate(' + (articleIndex) * -16.7 + '%)';
+
+}
+
+document.querySelectorAll('.works-carousel li').forEach(function(indicator, ind) {
+    indicator.addEventListener('click', function() {
+        articleIndex = ind;
+        setIndex();
+        indicator.classList.add('selected');
+
+    })
+})
+
+leftArrow.addEventListener('click', function() {
+    articleIndex = (articleIndex > 0) ? articleIndex - 1 : 0;
+    indicatorParents.children[articleIndex].classList.add('selected');
+    setIndex();
+});
+
+rightArrow.addEventListener('click', function() {
+    articleIndex = (articleIndex < 5) ? articleIndex + 1 : 5;
+    indicatorParents.children[articleIndex].classList.add('selected');
+    setIndex();
+});
